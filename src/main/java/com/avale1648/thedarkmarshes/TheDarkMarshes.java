9f744566d11fc7eea800f0ee44268a83ffa74e21
Андрей Миронов <avale1648@gmail.com>
2023-07-14 01:38:37 +0300

    InitialCommit

---
+++ src/main/java/com/avale1648/thedarkmarshes/TheDarkMarshes.java
@@ -0,0 +1,31 @@
+package com.avale1648.thedarkmarshes;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = TheDarkMarshes.MODID, name = TheDarkMarshes.NAME, version = TheDarkMarshes.VERSION)
+public class TheDarkMarshes
+{
+    public static final String MODID = "thedarkmarshes";
+    public static final String NAME = "The Dark Marshes";
+    public static final String VERSION = "1.0.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
